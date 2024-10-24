d122442db40b5f7a82f36d887478dff96e934137
aceofcolours <saavantandon@gmail.com>
2024-10-24 08:57:45 -0400

    constructors and print stats method

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,9 +1,34 @@
 ﻿using System;
 
+public class Character
+{
+    public string name;
+    public int exp;
+
+    public Character() //default constructor
+    {
+        name = "Not assigned";
+    }
+
+    public Character(string name)
+    {
+        this.name = name;
+    }
+
+    //methods
+
+    public void PrintStatsInfo()
+    {
+        Console.WriteLine("Hero: " + this.name + " - " + this.exp + " EXP");
+    }
+}
 class Program
 {
     public static void Main(string[] args)
     {
-        
+        Character hero = new Character();
+        hero.PrintStatsInfo();
+        Character heroine = new Character("Agatha");
+        heroine.PrintStatsInfo();
     }
 }
--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6090626021e7cc00f450a9f7962b458d0cde3f0d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a94410e2b562ac9ce682a05881be31d6b1f7d0f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
