be999c171a3a6d624ce70fd67c58e45cde0dc23b
aceofcolours <saavantandon@gmail.com>
2024-10-24 09:04:32 -0400

    struct constructor + methods

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -22,6 +22,25 @@ public class Character
         Console.WriteLine("Hero: " + this.name + " - " + this.exp + " EXP");
     }
 }
+
+public struct Weapon
+{
+    public string name;
+    public int damage;
+
+    public Weapon(string name, int damage)
+    {
+        this.name = name;
+        this.damage = damage;
+    }
+
+    //methods
+
+    public void PrintWeaponStats()
+    {
+        Console.WriteLine("Weapon: " + this.name + " - " + this.damage + " DMG");
+    }
+}
 class Program
 {
     public static void Main(string[] args)
@@ -30,5 +49,8 @@ class Program
         hero.PrintStatsInfo();
         Character heroine = new Character("Agatha");
         heroine.PrintStatsInfo();
+
+        Weapon huntingBow = new Weapon("Hunting Bow", 105);
+        huntingBow.PrintWeaponStats();
     }
 }
